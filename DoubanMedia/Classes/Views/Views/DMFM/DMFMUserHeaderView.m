2a84d68f4cc3437e080ca1975608710c1c1c0c7a
jsonmess <jsonmess@163.com>
2015-03-28 23:07:41 +0800

    自定义频道列表header

---
+++ DoubanMedia/Classes/Views/Views/DMFM/DMFMUserHeaderView.m
@@ -0,0 +1,13 @@
+//
+//  DMFMUserHeaderView.m
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/3/28.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import "DMFMUserHeaderView.h"
+
+@implementation DMFMUserHeaderView
+
+@end
