9816b0dd0080c6799a3187f09f01392c2cff1e3c
jsonmess <jsonmess@163.com>
2015-03-31 20:41:35 +0800

    添加音乐播放

---
+++ DoubanMedia/Classes/Controllers/DMFM/DBMusicPlayerController.h
@@ -0,0 +1,13 @@
+//
+//  DBMusicPlayerController.h
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/3/31.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBMusicPlayerController : UIViewController
+
+@end
---
+++ DoubanMedia/Classes/Controllers/DMFM/DBMusicPlayerController.m
@@ -0,0 +1,41 @@
+//
+//  DBMusicPlayerController.m
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/3/31.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import "DBMusicPlayerController.h"
+
+@interface DBMusicPlayerController ()
+
+@end
+
+@implementation DBMusicPlayerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+//设置目录----
+-(void)setUpView
+{
+
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
