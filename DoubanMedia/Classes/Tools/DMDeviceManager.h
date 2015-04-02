b70bf0837d6e97698d8abb46c4d5bfa1e55a7e47
jsonmess <jsonmess@163.com>
2015-04-03 00:04:16 +0800

    调整AlbumView位置

---
+++ DoubanMedia/Classes/Tools/DMDeviceManager.h
@@ -0,0 +1,13 @@
+//
+//  DMDeviceManager.h
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/4/1.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMDeviceManager : NSObject
+
+@end
---
+++ DoubanMedia/Classes/Tools/DMDeviceManager.m
@@ -0,0 +1,13 @@
+//
+//  DMDeviceManager.m
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/4/1.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import "DMDeviceManager.h"
+
+@implementation DMDeviceManager
+
+@end
