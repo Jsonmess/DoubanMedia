9bfccdb6607626cfbce14ca82f1d5a9be8dc9334
jsonmess <jsonmess@163.com>
2015-04-16 20:23:43 +0800

    解决状态栏动态隐藏的问题

---
+++ DoubanMedia/Classes/Views/Views/DMFilm/DMFilmListView.h
@@ -0,0 +1,13 @@
+//
+//  DMFilmListView.h
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/4/16.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMFilmListView : UIView
+
+@end
---
+++ DoubanMedia/Classes/Views/Views/DMFilm/DMFilmListView.m
@@ -0,0 +1,13 @@
+//
+//  DMFilmListView.m
+//  DoubanMedia
+//
+//  Created by jsonmess on 15/4/16.
+//  Copyright (c) 2015年 jsonmess. All rights reserved.
+//
+
+#import "DMFilmListView.h"
+
+@implementation DMFilmListView
+
+@end
